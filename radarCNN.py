f69b75042502540945f2404398d848ed26732d40
Hannah Hansen <hannhan@ntnu.no>
2022-10-13 14:39:34 +0200

    nn.Module to BaseFeatureEctractor

--- radarCNN.py
+++ radarCNN.py
@@ -6,9 +6,20 @@ import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
-class LidarCNN(nn.Module):
-    def __init__(self, n_sensors:int, output_channels:list, kernel_size:int=5):
-        super().__init__()
+
+class LidarCNN_pretrained(BaseFeaturesExtractor):
+    def __init__(self, 
+                 observation_space:gym.spaces.Box,
+                 output_channels:list = [4,4],
+                 kernel_size:int      = 9,
+                 n_sensors:int        = 180, 
+                 features_dim:int     =   1):
+
+        super(LidarCNN_pretrained, self).__init__(observation_space, features_dim=features_dim)
+        
+        self.mean = 146.29372782863646  # mean found during training
+        self.std  = 19.269065686163174  # standard deviation found during traning
+
         self.n_sensors = n_sensors
         self.kernel_size = kernel_size
         self.padding = (self.kernel_size - 1) // 2
@@ -54,12 +65,15 @@ class LidarCNN(nn.Module):
         )
 
     def forward(self, x):
-        
+        # Normalica sensor measurements before forward pass
+        x = (x - self.mean)/self.std
+
         for layer in self.feature_extractor:
             x = layer(x)
         for layer in self.regressor:
             x = layer(x)
         return x
+
 
 
 class NavigatioNN(BaseFeaturesExtractor):
@@ -80,11 +94,11 @@ class PerceptionNavigationExtractor(BaseFeaturesExtractor):
         This corresponds to the number of unit for the last layer.
     """
 
-    def __init__(self, observation_space: gym.spaces.Dict, sensor_dim : int = 180, features_dim: int = 32, kernel_overlap : float = 0.05):
+    def __init__(self, observation_space: gym.spaces.Dict, sensor_dim : int = 180, features_dim: int = 7, kernel_overlap : float = 0.05):
         # We do not know features-dim here before going over all the items,
         # so put something dummy for now. PyTorch requires calling
         # nn.Module.__init__ before adding modules
-        super(PerceptionNavigationExtractor, self).__init__(observation_space, features_dim=1)
+        super(PerceptionNavigationExtractor, self).__init__(observation_space, features_dim=features_dim)
         # We assume CxHxW images (channels first)
         # Re-ordering will be done by pre-preprocessing or wrapper
 
@@ -95,11 +109,16 @@ class PerceptionNavigationExtractor(BaseFeaturesExtractor):
         # so go over all the spaces and compute output feature sizes
         for key, subspace in observation_space.spaces.items():
             if key == "perception":
-                # Pass sensor readings through CNN
-                cnn = LidarCNN(n_sensors=sensor_dim, output_channels=[4,4], kernel_size=9)
-                cnn.load_state_dict(th.load('gym_auv/utils/cnn_1_pretrained.json'))
+
+                cnn = LidarCNN_pretrained(observation_space=subspace, 
+                                          n_sensors=sensor_dim, 
+                                          output_channels=[4,4], 
+                                          kernel_size=9,
+                                          features_dim=1)
+                                          
                 print('Loading pretrained LidarCNN')
-                print(cnn)
+                cnn.load_state_dict(th.load('gym_auv/utils/cnn_1_pretrained.json'))
+                
                 for param in cnn.parameters():
                     param.requires_grad = False # Freeze parameters. They will not be updated during backpropagation
                 
@@ -122,7 +141,7 @@ class PerceptionNavigationExtractor(BaseFeaturesExtractor):
         # self.extractors contain nn.Modules that do all the processing.
         for key, extractor in self.extractors.items():
             encoded_tensor_list.append(extractor(observations[key]))
-            print('Appended', key)
+            print('Appended' ,key)
         print(encoded_tensor_list)
         # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
         return th.cat(encoded_tensor_list, dim=1)
